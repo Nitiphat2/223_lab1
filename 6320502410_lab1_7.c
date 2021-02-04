37005f9b5302c583643b9aacacfe9e20b41d685d
Nitiphat2 <moozknot@gmail.com>
2021-02-04 17:20:49 +0700

    55

--- 6320502410_lab1_7.c
+++ 6320502410_lab1_7.c
@@ -4,20 +4,16 @@ int main()
     float time,a;
     scanf("%f",&time);
     printf("%f",time);
-    int b,d,c;
+    int b,c,d;
     if(time < 24)
     {
-        a = int(time) % 12;
-        b = a * 100;
-        c = floor(a);
-        d=b-c;
         if(0 < time && time < 12)
         {
             printf("%.2f am",time);
         }
         else if(12 < time && time < 24)
         {
-            printf("%d.%d pm",c,d);
+            printf("%.2f pm",time);
         }
     }
 
