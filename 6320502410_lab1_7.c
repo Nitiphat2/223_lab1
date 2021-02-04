441a14cb9438536d6c35e47d04d32f280d6e3f1a
Nitiphat2 <moozknot@gmail.com>
2021-02-04 16:56:13 +0700

    555

--- 6320502410_lab1_7.c
+++ 6320502410_lab1_7.c
@@ -1,1 +1,14 @@
-
+#include<stdio.h>
+int main()
+{
+    float time;
+    scanf("%f",&time);
+    if(0 < time && time < 12)
+    {
+        printf("%.2f am",time);
+    }
+    else if(12 < time && time < 24)
+    {
+        printf("%.2f pm",time);
+    }
+}
