80d14fd20c592b7c95f35e8248d6aa6c2da2130a
Nitiphat2 <moozknot@gmail.com>
2021-02-04 17:02:33 +0700

    owow

--- 6320502410_lab1_6.c
+++ 6320502410_lab1_6.c
@@ -3,49 +3,53 @@ int main()
 {
     int n,m,t,i=0,k=0;
     scanf("%d%d%d",&n,&m,&t);
-    char l[1];
-    scanf("%c ",&l);
-    if(l == 'L')
+    char l;
+    scanf(" %c",&l);
+    if(n < 1000 && m < 1000 && (t > 1 && t <1000000000))
     {
-        while(t > 0)
+        if(l == 'L')
         {
-            i++;
-            if(i%2==1)
+            while(t > 0)
             {
-                t=t-n;
-                k=0;
-            }
-            else
-            {
-                t=t-m;
-                k=1;
+                i++;
+                if(i%2==1)
+                {
+                    t=t-n;
+                    k=0;
+                }
+                else
+                {
+                    t=t-m;
+                    k=1;
+                }
             }
         }
-    }
-    else if(l == 'R')
-    {
-        while(t > 0)
+        else if(l == 'R')
         {
-            i++;
-            if(i%2==0)
+            while(t > 0)
             {
-                t=t-n;
-                k=0;
-            }
-            else
-            {
-                t=t-m;
-                k = 1;
+                i++;
+                if(i%2==0)
+                {
+                    t=t-n;
+                    k=0;
+                }
+                else
+                {
+                    t=t-m;
+                    k = 1;
+                }
             }
         }
+
+        if(k == 0)
+        {
+            printf("L");
+        }
+        else
+        {
+            printf("R");
+        }
     }
-    printf("%d",k);
-    if(k == 0)
-    {
-        printf("L");
-    }
-    else
-    {
-        printf("R");
-    }
+
 }
